d1dec01a81da6fad15ec566b627240ae4686b290
HULIANGA <835710406@qq.com>
2016-10-20 16:30:47 +0800

    修改跳转链接function

--- src/js/base.js
+++ src/js/base.js
@@ -3,6 +3,17 @@ window.ctx = '../..'
 /************** 图片路径 ****************/
 window.imageDomain = 'http://meimeidou.qiniudn.com/'
 /************ 地址跳转经过后台 ***********/
+
+// mkj001
+// var webOrigin = 'http://mkj001.bigaka.com/mkj'
+// var webPath = '/mkj/view/html/'
+// 本地
+var webOrigin = 'http://mkj001.bigaka.com/mkj'
+var webPath = '/dist/html/'
+// 生产
+// var webOrigin = 'http://mkj001.bigaka.com/mkj'
+// var webPath = '/dist/html/'
+
 window.goPage = function (url) {
-  window.location.href = '../../mkj/main?param=' + encodeURIComponent(url)
+  window.location.href = webOrigin + '/main?param=' + encodeURIComponent(window.location.origin + webPath + url)
 }
