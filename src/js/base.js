d34193b36458013626f8f01dc0c61ebd00164c12
HULIANGA <835710406@qq.com>
2016-10-20 14:05:07 +0800

    修改页面跳转function

--- src/js/base.js
+++ src/js/base.js
@@ -4,5 +4,5 @@ window.ctx = '../..'
 window.imageDomain = 'http://meimeidou.qiniudn.com/'
 /************ 地址跳转经过后台 ***********/
 window.goPage = function (url) {
-  window.location.href = 'http://mkj001.bigaka.com/mkj/main?param=' + encodeURIComponent(window.location.origin + '/dist/html/' + url)
+  window.location.href = '../../mkj/main?param=' + encodeURIComponent(url)
 }
