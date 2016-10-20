a1fba54d3b6d4fa303d2fd6a28be19fae4e7d4bc
HULIANGA <835710406@qq.com>
2016-10-20 10:19:56 +0800

    增加页面跳转方法

--- src/js/base.js
+++ src/js/base.js
@@ -1,4 +1,8 @@
 /************** ajax路径 ***************/
 window.ctx = '../..'
-/************** 图片路径 ***************/
+/************** 图片路径 ****************/
 window.imageDomain = 'http://meimeidou.qiniudn.com/'
+/************ 地址跳转经过后台 ***********/
+window.goPage = function (url) {
+  window.location.href = 'http://mkj001.bigaka.com/mkj/main?param=' + encodeURIComponent(window.location.origin + '/dist/html/' + url)
+}
