398c5b5373a75f683890bde711e770cccc2f5c48
HULIANGA <835710406@qq.com>
2016-11-03 09:46:36 +0800

    修改后台跳转路径正式环境配置

--- src/js/base.js
+++ src/js/base.js
@@ -1,19 +1,17 @@
-/************** ajax路径 ***************/
+/********************************* ajax路径 ***********************************/
 window.ctx = '../..'
-/************** 图片路径 ****************/
+/********************************* 图片路径 ************************************/
 window.imageDomain = 'http://meimeidou.qiniudn.com/'
-/************ 地址跳转经过后台 ***********/
-
-// mkj001
-// var webOrigin = 'http://mkj001.bigaka.com/mkj'
-// var webPath = '/mkj/view/html/'
+/*************************** 页面跳转经过后台重定向 ******************************/
 // 本地
 var webOrigin = 'http://mkj001.bigaka.com/mkj'
 var webPath = '/dist/html/'
-// 生产
+// mkj001
 // var webOrigin = 'http://mkj001.bigaka.com/mkj'
-// var webPath = '/dist/html/'
-
+// var webPath = '/mkj/view/html/'
+// 生产
+// var webOrigin = 'https://app.dymkj.net/mkj'
+// var webPath = '/mkj/dist/html/'
 window.goPage = function (url) {
   window.location.href = webOrigin + '/main?param=' + encodeURIComponent(window.location.origin + webPath + url)
 }
