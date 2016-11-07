878ba2d8276e5a5aa4bb6213a751c1495ba13d8d
HULIANGA <835710406@qq.com>
2016-11-07 15:24:46 +0800

    增加growingIo统计代码

--- src/js/base.js
+++ src/js/base.js
@@ -15,3 +15,21 @@ var webPath = '/dist/html/'
 window.goPage = function (url) {
   window.location.href = webOrigin + '/main?param=' + encodeURIComponent(window.location.origin + webPath + url)
 }
+// javascript.js
+var head = document.head || document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.innerHTML =
+  "var _vds = _vds || [];"+
+      "window._vds = _vds;"+
+      "(function(){"+
+        "_vds.push(['setAccountId', '8f07dd4da9e64e3d']);"+
+        "(function() {"+
+          "var vds = document.createElement('script');"+
+          "vds.type='text/javascript';"+
+          "vds.async = true;"+
+          "vds.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'dn-growing.qbox.me/vds.js';"+
+          "var s = document.getElementsByTagName('script')[0];"+
+          "s.parentNode.insertBefore(vds, s);"+
+        "})();"+
+      "})();";
+head.appendChild(script);
---
+++ test.js
@@ -0,0 +1,31 @@
+{
+  html: '<!DOCTYPE html>\n<html>\n  <head>\n    <meta charset="utf-8">\n    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no" />\n    <meta name="apple-touch-fullscreen" content="yes" />\n    <meta name="apple-mobile-web-app-capable" content="yes" />\n    <meta name="apple-mobile-web-app-status-bar-style" content="black" />\n    <meta name="format-detection" content="telephone=no" />\n    <meta name="apple-mobile-web-app-title" content="美空间"/>\n    <title>注册</title>\n  </head>\n  <body>\n    <old-regist></old-regist>\n  </body>\n</html>\n',
+  assets:
+   { publicPath: 'http://localhost:8080/dist/',
+     chunks: { oldRegist: [Object] },
+     js: [ 'http://localhost:8080/dist/js/oldRegist.8ad5de9531c201fba55f.js' ],
+     css: [ 'http://localhost:8080/dist/css/oldRegist.8ad5de9531c201fba55f.css' ],
+     manifest: undefined
+   },
+  plugin:
+   HtmlWebpackPlugin {
+     options:
+      { template: '/Users/huhuliang/git/MeiKJ/node_modules/html-webpack-plugin/lib/loader.js!/Users/huhuliang/git/MeiKJ/src/html/oldRegist.html',
+        filename: 'html/oldRegist.html',
+        hash: false,
+        inject: true,
+        compile: true,
+        favicon: false,
+        minify: false,
+        cache: true,
+        showErrors: true,
+        chunks: [Object],
+        excludeChunks: [],
+        title: 'Webpack App',
+        xhtml: false
+      },
+     childCompilerHash: '8965ffd3f22f62bd8ecc77ab8ebbc224',
+     childCompilationOutputName: 'html/oldRegist.html',
+     assetJson: '["http://localhost:8080/dist/","http://localhost:8080/dist/css/oldRegist.8ad5de9531c201fba55f.css","http://localhost:8080/dist/js/oldRegist.8ad5de9531c201fba55f.js"]' },
+  outputName: 'html/oldRegist.html'
+}
